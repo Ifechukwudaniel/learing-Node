427eca9b60c807241cfc961ce2f56a77ca29d747
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-29 17:30:42 +0100

    added axios

--- axios.js
+++ axios.js
@@ -1,1 +1,8 @@
 const axios = require("axios")
+const username = "CodesDaniel"
+
+axios.get(`https://api.github.com/users/${username}`).then(res=>{
+  console.log(res.data.name)
+}).catch((err)=>{
+    console.log(err)
+})

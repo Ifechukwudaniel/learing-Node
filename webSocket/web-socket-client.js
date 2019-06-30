321941bf7012a22a2db8d025ce2339278db5042b
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-30 20:34:21 +0100

    web socket with node

--- webSocket/web-socket-client.js
+++ webSocket/web-socket-client.js
@@ -1,7 +1,28 @@
 const WS = new WebSocket("ws://localhost:3000")
 
+WS.onmessage= (payload)=>{
+  AddMessage(payload.data)
+}
+WS.onopen= ()=>{
+    DisplayTitle("CONNECTED TO SERVER")
+}
+WS.onclose= ()=>{
+    DisplayTitle("DISCONNECTED FROM SERVER")
+}
+
+
 const handleSubmit =()=>{
     let message = document.getElementById("message").value
    //  console.log(message)
     WS.send(message)
+}
+
+const DisplayTitle= ( title)=>{
+    document.querySelector("h1").innerHTML = title
+}
+
+const AddMessage = title =>{
+    let h1 = document.createElement("p")
+    h1.innerHTML= title
+    document.querySelector("div.message").append(h1)
 }
--- webSocket/web-socket.js
+++ webSocket/web-socket.js
@@ -5,6 +5,9 @@ const WSS = new WebSocketServer({port: 3000})
 WSS.on("connection",(ws)=>{
     console.log("you  are connected//")
     ws.on("message", (data)=>{
-        console.log(data)
+            
+        WSS.clients.forEach((client)=>{
+            client.send(data)
+         })
     })
 })
